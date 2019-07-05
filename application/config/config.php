dc721931f3d7c3a062dcf174cd5f35a10ba803cd
rizkyferdian04 <rizky_ferdian_26rpl@student.smktelkom-mlg.sch.id>
2019-07-05 10:51:47 +0700

    ganti base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/boogja';
+$config['base_url'] = 'http://localhost/pkl-telkom-malang';
 
 /*
 |--------------------------------------------------------------------------
