9f03c747c5458502ea8736791615aacf0ad6e437
fanirahmat <fanirahmatulloh842@gmail.com>
2019-07-10 14:22:20 +0700

    ok

--- application/modules/admin/models/Verif_model.php
+++ application/modules/admin/models/Verif_model.php
@@ -9,18 +9,26 @@ class Verif_model extends CI_Model {
 	}
 	public function get_nota()
 	{
-		$query = $this->db
-							->join('tb_order', 'tb_order.id_order = detail_order.id_order')
-		
+		if ($this->session->userdata('nama_level')=="admin") 
+		{
+			$query = $this->db->join('tb_order', 'tb_order.id_order = detail_order.id_order')
 						->join('pelanggan','pelanggan.id_pelanggan=tb_order.id_pelanggan')
-						
+						->join('restoran','restoran.id_restoran=detail_order.id_restoran')
+						->join('masakan','masakan.id_masakan=detail_order.id_masakan')
+						->get('detail_order')->result();
+			return $query;
+		}
+		else
+		{
+			$query = $this->db->join('tb_order', 'tb_order.id_order = detail_order.id_order')
+						->join('pelanggan','pelanggan.id_pelanggan=tb_order.id_pelanggan')
 						->join('restoran','restoran.id_restoran=detail_order.id_restoran')
 						->where('restoran.id_restoran', $this->session->userdata('id_restoran'))
-						
 						->join('masakan','masakan.id_masakan=detail_order.id_masakan')
-						
 						->get('detail_order')->result();
-		return $query;
+			return $query;
+		}
+		
 	}
 	public function detail($id_order)
 	{
