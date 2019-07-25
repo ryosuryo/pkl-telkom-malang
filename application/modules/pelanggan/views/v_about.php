0554f607fdaf90fb18c42bd3d569ca4d050faf2f
rizkyferdian04 <rizky_ferdian_26rpl@student.smktelkom-mlg.sch.id>
2019-07-25 11:02:24 +0700

    tampilan

--- application/modules/pelanggan/views/v_about.php
+++ application/modules/pelanggan/views/v_about.php
@@ -6,16 +6,11 @@
         <div class="col-md-12 col-lg-7">
           <div class="title-single-box">
             <h1 class="title-single">Apa itu Boogja ?</h1>
-            <span class="color-text-a"><img src="<?= base_url()?>assets2/img/speaker.png" style="width: 4%">  Boogja, singkatan dari "Booking Saja" adalah sebuah aplikasi berbasis web yang bertujuan untuk memudahkan para pelanggan untuk memesan meja di restoran favorit sekaligus memesan masakan di restoran yang dipilih secara online, sehingga para pelanggan tidak perlu datang langsung ke tempat restoran.
-            <br>
-            <br>
-            <h1 class="title-single">Kenapa Membuat Boogja ?</h1>
-            Pada zaman ini banyak orang memilih restoran sebagai salah satu tempat berkumpul Bersama keluarga , seperti mengadakan makan bareng keluarga atau bareng teman teman.
-
-Akan tetapi  keterbatasan tempat atau meja makan di restoran-restoran yang tak sebanding dengan jumlah pengunjung membuat banyak pengunjung di restoran tersebut kecewa.
-
-Jumlah pengunjung yang membludak pada waktu waktu tertentu membuat pihak restoran kerepotan. Meskipun sudah ada layanan booking meja dibanyak restoran, tetapi cara yang digunakan masih offline atau pemesan harus datang ketempat restoran terlebih dahulu untuk memesan meja.
-
+            <span class="color-text-a"><img src="<?= base_url()?>assets2/img/speaker.png" style="width: 4%">  
+            <p align ="justify">
+            Boogja, singkatan dari "Booking Saja" adalah sebuah aplikasi berbasis web yang bertujuan untuk memudahkan para pelanggan untuk memesan meja di restoran favorit sekaligus memesan masakan di restoran yang dipilih secara online, sehingga para pelanggan tidak perlu datang langsung ke tempat restoran.
+            </p>
+           
             
             
             </span>
@@ -33,14 +28,19 @@ Jumlah pengunjung yang membludak pada waktu waktu tertentu membuat pihak restora
       <div class="row">
         
         <div class="col-md-12 col-lg-6">
-          <img src="<?= base_url()?>assets2/img/akhwat1.png" style="width: 100%;">
+          <img src="<?= base_url()?>assets2/img/boggja.png" style="width: 100%;">
         </div>
         <div class="col-md-12 col-lg-6">
           <div class="title-single-box">
-            <h1 class="title-single">Dengan Adanya Boogja</h1>
-            <span class="color-text-a"><img src="<?= base_url()?>assets2/img/speaker.png" style="width: 4%">  
-            Pesan meja dan makanan dapat lebih mudah dan membantu para masyarakat memesan meja sekaligus makanan secara online
+            <h1 class="title-single">Kenapa Membuat Boogja ?</h1>
+            <p align="justify">
+            Pada zaman ini banyak orang memilih restoran sebagai salah satu tempat berkumpul Bersama keluarga , seperti mengadakan makan bareng keluarga atau bareng teman teman.
 
+Akan tetapi  keterbatasan tempat atau meja makan di restoran-restoran yang tak sebanding dengan jumlah pengunjung membuat banyak pengunjung di restoran tersebut kecewa.
+
+Jumlah pengunjung yang membludak pada waktu waktu tertentu membuat pihak restoran kerepotan. Meskipun sudah ada layanan booking meja dibanyak restoran, tetapi cara yang digunakan masih offline atau pemesan harus datang ketempat restoran terlebih dahulu untuk memesan meja.
+</p>
+          
             </span>
           </div>
         </div>
@@ -48,6 +48,29 @@ Jumlah pengunjung yang membludak pada waktu waktu tertentu membuat pihak restora
     </div>
   </section>
   
+
+
+  <section class="intro-single">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 col-lg-6">
+          <div class="title-single-box">
+            <h1 class="title-single">Dengan Adanya Boogja</h1>
+            <span class="color-text-a"><img src="<?= base_url()?>assets2/img/speaker.png" style="width: 4%">  
+           <p align="justify">
+            Pesan meja dan makanan dapat lebih mudah dan membantu para masyarakat memesan meja sekaligus makanan secara online
+            </p>  
+            
+            
+            </span>
+          </div>
+        </div>
+        <div class="col-md-12 col-lg-6">
+          <img src="<?= base_url()?>assets2/img/akhwat.png" style="width: 100%;">
+        </div>
+      </div>
+    </div>
+  </section>
 
   
  
