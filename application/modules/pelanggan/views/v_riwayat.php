b70c05ad902a64280e9fe673d94cf2090fc90861
fanirahmat <fanirahmatulloh842@gmail.com>
2019-07-26 14:35:45 +0700

    asdf

--- application/modules/pelanggan/views/v_riwayat.php
+++ application/modules/pelanggan/views/v_riwayat.php
@@ -5,7 +5,6 @@
         <div class="col-md-12 col-lg-8">
           <div class="title-single-box">
             <h1 class="title-single">Riwayat Booking</h1>
-            <span class="color-text-a"><----------------!></span>
           </div>
         </div>
         <div class="col-md-12 col-lg-4">
